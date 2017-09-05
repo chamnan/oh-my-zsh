254a76019c8b97b80fb4d6d043efdbd352e41756
Chamnan <chamnan.nop@gmail.com>
2017-09-05 10:06:40 +0700

    Update git.plugin.zsh
    Add alias glgp

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -58,6 +58,8 @@ alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=10'
 compdef _git glg=git-log
+alias glgp='git log --stat -p'
+compdef _git glgp=git-log
 alias glgg='git log --graph --max-count=10'
 compdef _git glgg=git-log
 alias glgga='git log --graph --decorate --all'
